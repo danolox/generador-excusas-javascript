b7106ae4ac050f7d62c85042a337a2550d2904c0
Daniel <danolox@gmail.com>
2025-04-14 10:47:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 
 
 
-let alguien = ['Mi vecino', 'Mi perro', 'Mi hermano', 'Mi jefe', 'Mi sobrino', 'Mi hamster', 'Mi compañero de clase', 'Mi abuelo', 'El panadero del barrio', 'El frutero de la esquina'];
+const alguien = ['Mi vecino', 'Mi perro', 'Mi hermano', 'Mi jefe', 'Mi sobrino', 'Mi hamster', 'Mi compañero de clase', 'Mi abuelo', 'El panadero del barrio', 'El frutero de la esquina'];
 
-let verbo = ['se comió', 'tiró por la ventana', 'quemó', 'tiró en la piscina', 'rompió con un bate de béisbol', 'pulverizó', 'apostó en una partida de póker', 'horneó'];
+const verbo = ['se comió', 'tiró por la ventana', 'quemó', 'tiró en la piscina', 'rompió con un bate de béisbol', 'pulverizó', 'apostó en una partida de póker', 'horneó'];
 
-let objeto = ['mi presentación', 'mi pasaporte', 'el informe que debo entregar', 'el examen que yo tenía que entregar', 'mi billetera'];
+const objeto = ['mi presentación', 'mi pasaporte', 'el informe que debo entregar', 'el examen que yo tenía que entregar', 'mi billetera'];
 
-let quasiAdverbio = ['de forma apresurada', 'sin pestañear', 'porque sí', 'sin yo saberlo', 'sin darse cuenta'];
+const quasiAdverbio = ['de forma apresurada', 'sin pestañear', 'porque sí', 'sin yo saberlo', 'sin darse cuenta'];
 
-let tiempo = ['esta mañana', 'a mediodía', 'durante mi descanso', 'mientras yo dormía'];
+const tiempo = ['esta mañana', 'a mediodía', 'durante mi descanso', 'mientras yo dormía'];
 
-let lugar = ['en la oficina', 'en mi cocina', 'en la casa del vecino', 'en el parque' ];
+const lugar = ['en la oficina', 'en mi cocina', 'en la casa del vecino', 'en el parque' ];
 
 
 function randomizador (lista) {
-
   let tope = lista.length;
   let numeroRandom = Math.floor(Math.random() * tope);
   return lista[numeroRandom];
